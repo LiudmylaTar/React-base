f07feead47a23fe1f0f4f5c0f09d73a560922196
Liudmyla_Taran <taran.lyudok@gmail.com>
2025-09-13 13:27:30 +0300

    form-remove

--- src/tabs/TasksTab.tsx
+++ src/tabs/TasksTab.tsx
@@ -1,3 +1,10 @@
+const initialTasks = [
+  { id: "id-1", task: "Create repositore" },
+  { id: "id-2", task: "Create progect" },
+  { id: "id-3", task: "Create counter component" },
+  { id: "id-4", task: "Create tasck list" },
+];
+
 export default function TasksTab() {
   return (
     <>
