d069950a5a2eb165ebaf1e5d37f059c0548c388c
Liudmyla_Taran <taran.lyudok@gmail.com>
2025-09-15 14:05:19 +0300

    tasks

--- src/components/FormAddTask/FormAddTask.tsx
+++ src/components/FormAddTask/FormAddTask.tsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import "./FormAddTask.css";
 
-export default function FormAddTask({ onSubmit }) {
+interface FormAddTaskProps {
+  onSubmit: (taskText: string) => void;
+}
+
+export default function FormAddTask({ onSubmit }: FormAddTaskProps) {
   const [value, setValue] = useState("");
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (!value.trim) return;
-    onSubmit(value);
+    if (!value.trim()) return;
+    onSubmit(value.trim());
     setValue("");
   };
-  const handleChange = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
   return (
---
+++ src/components/TaskList/TaskList.tsx
@@ -0,0 +1,19 @@
+import type { Task } from "../../types";
+import TaskItem from "../TaskItem/TaskItem";
+
+interface TasksListProps {
+  startTasks: Task[];
+  onToggle: (id: string) => void;
+}
+
+export default function TasksList({ startTasks, onToggle }: TasksListProps) {
+  return (
+    <ul className="list">
+      {startTasks.map((task) => (
+        <li className="item" key={task.id}>
+          <TaskItem data={task} onToggle={onToggle} />
+        </li>
+      ))}
+    </ul>
+  );
+}
